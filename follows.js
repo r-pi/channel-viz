5cae5e08d209f9c8e0f01e789daccb79a67b221e
r-pi <r-pi@charles.net.nz>
2013-09-29 11:28:04 +1300

    set appName, dataDuration, hideForm

--- follows.js
+++ follows.js
@@ -14,11 +14,11 @@
 
 	var defaultKey		= 'pSOUFpJxkRwTB750tMMzf6DDF9tdg93orlPA1dzhF0QInDLS', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [101758294], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // 7days Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		applicationName	= 'Poraiti Pi Dashboard', // Replaces Xively logo in the header
+		dataDuration	= '7days', // 7days Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
