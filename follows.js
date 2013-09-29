234a926cfb0548e1153d29e8ea003d5133d4b446
r-pi <r-pi@charles.net.nz>
2013-09-29 13:44:16 +1300

    Modifying updateFeeds fn to accept duration of 3days

--- follows.js
+++ follows.js
@@ -85,9 +85,9 @@
 					var diff = null;
 					if(duration == '6hours') diff = 21600000;
 					 if(duration == '1day') diff = 86400000;
+					 if(duration == '3days') diff = 259200000;
 					 if(duration == '1week') diff = 604800000;
 					 if(duration == '1month') diff = 2628000000;
-					 if(duration == '90days') diff = 7884000000;
 					then.setTime(now.getTime() - diff);
 					if(updated.getTime() > then.getTime()) {
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {
